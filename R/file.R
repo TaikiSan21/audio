ef8cb683125883b9d7b5eb4bb6b3859cf135cc37
Taiki <tnsakai@gmail.com>
2024-07-10 16:18:41 -0700

    adding first changes to stop crashing on our wav files and adding from and to params

--- R/file.R
+++ R/file.R
@@ -1,4 +1,8 @@
-load.wave <- function(where) invisible(.Call(load_wave_file, where, PACKAGE="audio"))
+load.wave <- function(where, from=0, to=NA_real_) {
+    from <- as.numeric(from)
+    to <- as.numeric(to)
+    invisible(.Call(load_wave_file, where, from, to, PACKAGE="audio"))
+}
 
 save.wave <- function(what, where) invisible(.Call(save_wave_file, where, what, PACKAGE="audio"))
 
